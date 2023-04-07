dca2f28c70f8dbc979fbe529d850aa5f750d7bed
mancuoj <1337381768@qq.com>
2023-04-07 21:37:17 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,6 @@
+# PlayReact's Homepage
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/c3e9a52c-b179-4886-869b-e70ca90767a5/deploy-status)](https://app.netlify.com/sites/playreact-template/deploys)
+![Star](https://img.shields.io/github/stars/playreact/home?style=flat)
+![Fork](https://img.shields.io/github/forks/playreact/home?style=flat)
+![MIT](https://img.shields.io/github/license/playreact/home?style=flat)
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen justify-center items-center flex-col gap-8 font-serif">
+      <h1 className="font-bold text-5xl text-info">Play React</h1>
+      <p className="text-2xl">A collection of fun and engaging React-based games 🕹️</p>
+      <a href="https://github.com/playreact" className="btn">
+        view on github
+      </a>
+    </div>
+  )
+}
