6f03504bde609915e053a5b245236cf5be4a0a7c
mancuoj <1337381768@qq.com>
2023-04-08 18:17:25 +0800

    style: update basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 export default function App() {
   return (
-    <div className="flex min-h-screen justify-center items-center flex-col gap-8 font-serif">
-      <h1 className="font-bold text-5xl text-info">Play React</h1>
-      <p className="text-2xl">A collection of fun and engaging React-based games 🕹️</p>
-      <a href="https://github.com/playreact" className="btn">
+    <div className="p-5 flex min-h-screen justify-center items-center flex-col gap-8 font-serif">
+      <h1 className="font-bold text-4xl text-info">
+        Play React
+      </h1>
+      <p className="text-xl text-center">
+        A collection of React-based games 🕹️
+      </p>
+      <a href="https://github.com/playreact" className="btn btn-info btn-outline btn-md">
         view on github
       </a>
     </div>
