79a154e05d5e9df0a68f1e134df5283bbfc291b3
mancuoj <1337381768@qq.com>
2023-04-19 23:20:21 +0800

    feat: update slogan and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 export default function App() {
   return (
     <div className="p-5 flex min-h-screen justify-center items-center flex-col gap-8 font-serif">
-      <h1 className="font-bold text-4xl text-info">
+      <h1 className="font-bold text-4xl text-success">
         Play React
       </h1>
       <p className="text-xl text-center">
-        A collection of React-based games 🕹️
+        A collection of React-based apps 🕹️
       </p>
-      <a href="https://github.com/playreact" className="btn btn-info btn-outline btn-md">
+      <a href="https://github.com/playreact" className="btn btn-success btn-outline btn-md">
         view on github
       </a>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,8 @@ export default {
     extend: {},
   },
   plugins: [require('daisyui')],
-  daisyui: {},
+  daisyui: {
+    themes: ['lofi', 'forest'],
+    darkTheme: 'forest',
+  },
 }
