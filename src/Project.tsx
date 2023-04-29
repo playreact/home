8556dd4b479251571d2d4caede2de22f8472cbe2
mancuoj <1337381768@qq.com>
2023-04-30 00:55:48 +0800

    use div

--- src/Project.tsx
+++ src/Project.tsx
@@ -9,8 +9,8 @@ const Project: React.FC<ProjectProps> = ({ name, description, stars, language })
   return (
     <a href={`https://github.com/playreact/${name}`} target="_blank">
       <div className="h-36 p-5 flex flex-col justify-between rounded-xl bg-info bg-opacity-5 shadow hover:shadow-lg hover:bg-opacity-10 ease-out transition-transform">
-        <p className="font-semibold">{name}</p>
-        <p className="text-sm">{description}</p>
+        <div className="font-semibold">{name}</div>
+        <div className="text-sm">{description}</div>
         <div className="flex gap-2 items-center text-sm">
           <div>⭐ {stars}</div>
           <div>{language}</div>
